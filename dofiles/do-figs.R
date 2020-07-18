69193a30dfc898575944634520c9e318e7a8ce2b
Alain <vando026@umn.edu>
2020-07-18 10:54:14 +0200

    Add do-figs

---
+++ dofiles/do-figs.R
@@ -0,0 +1,169 @@
+## Description: Do figures
+## Project: CoVideo
+## Author: AV / Created: 18Jul2020 
+
+######################################################################
+######################### CONSORT ####################################
+######################################################################
+cdat <- readr::read_csv(file.path(datapath, "Derived", "CONSORT.csv"))
+
+getStats <- function(x) {
+  dat <- cdat[grepl(x, cdat$Var), -1]
+  sum(apply(dat, 1, function(x) sum(x, na.rm=TRUE)))
+}
+nms <- c("In", "Start", "Trt", "APC", "End")
+flow <- lapply(setNames(nms, nms), getStats)
+flow
+
+fdat <- select(dat_all, ID, VideoArm, TreatList)
+fdat <- data.frame(with(fdat, table(VideoArm, TreatList)))
+# Total completed
+sum(fdat$Freq)
+fdat_sum <- tapply(fdat$Freq, fdat$VideoArm, sum)
+  
+# Total enrolled
+Total_In <- flow$In
+# Lost at Consent
+First_Lost <- flow$Start
+# Total going into arms
+ArmsN <- Total_In - First_Lost
+CoVidN <- fdat_sum["Treatment"] + flow$Trt
+APCN <- fdat_sum["Placebo"] + flow$APC
+CtrlN <- ArmsN - CoVidN - APCN
+# Lost at Crtl
+Ctrl_Lost <- CtrlN - fdat_sum["Control"] 
+# Total enrolled into Arms
+ArmsN == (APCN + CtrlN + CoVidN)
+
+# Checks 
+(CoVidN - flow$Trt) == fdat_sum["Treatment"]
+(APCN - flow$APC) == fdat_sum["Placebo"]
+(CtrlN - Ctrl_Lost) == fdat_sum["Control"]
+
+######################################################################
+###################### Plots #########################################
+######################################################################
+pbrack <- function(x0, x1, y, h=0.01, pval="p < 0.05", ...) {
+  segments(x0, y, x1, y, ...)
+  segments(x0, y-h, x0, y, ...)
+  segments(x1, y-h, x1, y, ...)
+  text((x0 + x1)/2, y + (h*0.8), pval)
+}
+fmt <- function(x) {
+  x <- formatC(x, format="f", digits=3)
+  ifelse(x=="0.000", "p < 0.001", paste0("p = ", x))
+}
+
+dif1 <- function(x) {
+  y <- list(cv = x[3]-x[1], ac = x[2]-x[1], av = x[3]-x[2])
+  lapply(y, function(x) paste0("Diff = ", formatC(x, format="f", digits=3)))
+}
+
+######################################################################
+######################### KNowledge ##################################
+######################################################################
+adat <- filter(dat_all, !is.na(ClinicTotal) | !is.na(SpreadTotal)) %>% 
+  select(ClinicTotal, SpreadTotal, VideoArm)
+  
+cmod <- lm(ClinicTotal ~ -1 + VideoArm, data=adat)
+cpair <- with(adat, pairwise.t.test(ClinicTotal, VideoArm, p.adj = "none"))
+y <- cmod$coefficients
+cis <- confint(cmod)
+lis <- y - cis[, 1] 
+uis <- cis[, 2] - y
+pvals <- fmt(cpair$p.value)
+difc <- dif1(y)
+
+png(file.path(output, "Know_Clinic.png"),
+  units="in", width=5, height=5.0, pointsize=9, 
+  res=500, type="cairo")
+plotCI(1:3, y, liw=lis, uiw=uis, main="Knowledge (Clinical)", 
+  bty="n", ylim=c(9.15, 9.39), lwd=2, pch=16, font.lab=2,
+  xlab="Trial arm", ylab="Mean score", xaxt="n")
+axis(1, 1:3, c("Control", "APC", "CoVideo"))
+pbrack(1, 2, 9.29, pval=paste0(difc$ac, ", ", pvals[1, 1]))
+pbrack(2, 3, 9.34, pval=paste0(difc$av, ", ", pvals[2, 2]))
+pbrack(1, 3, 9.38, pval=paste0(difc$cv, ", ", pvals[2, 1]))
+dev.off()
+
+cmod <- lm(SpreadTotal ~ -1 + VideoArm, data=adat)
+cpair <- with(adat, pairwise.t.test(SpreadTotal, VideoArm, p.adj = "none"))
+y <- cmod$coefficients
+cis <- confint(cmod)
+lis <- y - cis[, 1] 
+uis <- cis[, 2] - y
+pvals <- fmt(cpair$p.value)
+difs <- dif1(y)
+
+png(file.path(output, "Know_Spread.png"),
+  units="in", width=5, height=5.0, pointsize=9, 
+  res=500, type="cairo")
+plotCI(1:3, y, liw=lis, uiw=uis, main="Knowledge (Spread)", 
+  bty="n", ylim=c(8.40, 8.55), lwd=2, pch=16, font.lab=2,
+  xlab="Trial arm", ylab="Mean score", xaxt="n")
+axis(1, 1:3, c("Control", "APC", "CoVideo"))
+pbrack(1, 2, 8.48, h=0.005, pval=paste0(difs$ac, ", ", pvals[1, 1]))
+pbrack(2, 3, 8.51, h=0.005, pval=paste0(difs$av, ", ", pvals[2, 2]))
+pbrack(1, 3, 8.54, h=0.005, pval=paste0(difs$cv, ", ", pvals[2, 1]))
+dev.off()
+
+######################################################################
+######################### Behave Direct ##############################
+######################################################################
+# Function to wrap statements
+bwrap <- function(y) paste(strwrap(bstate[y], 14), collapse="\n")
+
+# Function to get means and cis
+doReg <- function(RHS, dat) {
+  function(LHS) {
+    mod <- lm(as.formula(paste(LHS, RHS)), data=dat)
+    cf <- round(coef(mod)[2]*100, 1)
+    ci <- round(confint(mod)[2, ]*100, 1) 
+    return(c(cf, ci))
+  }
+}
+doReg <- doReg_(RHS = " ~   TreatList", bdat)
+
+# Make the data 
+ldat <- getListData(dat_all)
+bdat <- filter(ldat, VideoArm=="Control") 
+behav = data.frame(t(sapply(names(bstate), doReg)))
+behav <- arrange(behav, TreatList1)
+names(behav) <- c("Est", "LB", "UB")
+bnames <- lapply(rownames(behav), bwrap)
+
+png(file.path(output, "BehavIntent.png"),
+  units="in", width=6.5, height=5.0, pointsize=9, 
+  res=500, type="cairo")
+bp <- barplot(behav$Est, xaxt="n", ylim=c(0, 100),
+  main="This week I will, ...", ylab="Prevalence", font.lab=2,
+  col="darkslategray3", border="darkslategray4")
+text(x = bp, y=-1, label=bnames, xpd=TRUE, 
+     adj=c(0.5, 1), cex=0.8, srt=0)
+text(x = bp-0.05, y=behav$Est+1, label=behav$Est,
+     adj=c(1, 0), offset=0.5, cex=0.8)
+plotCI(bp, behav$Est, li=behav$LB, ui=behav$UB, 
+  main="Behavioral Intent", bty="n", add=TRUE,
+  ylim=c(0, 100), lwd=1, pch=16, 
+  xlab="Trial arm", xaxt="n", col="gray30")
+dev.off()
+
+######################################################################
+####################### PLot Behav means #############################
+######################################################################
+doReg2 <- doReg_(RHS = "~ -1 + VideoArm:TreatList", ldat)
+
+doReg2("SocialDist")
+
+plotBar <- function(dat) {
+  barplot(as.matrix(dat[, c(1, 7, 3, 9, 5, 11)]))
+}
+plotBar(tablist[1, ])
+
+
+tapply(ldat2$SocialDist, list(ldat2$VideoArm, ldat2$TreatList), mean)
+tapply(ldat$SocialDist, list(ldat$VideoArm, ldat$TreatList), mean)
+
+group_by(ldat, VideoArm, TreatList) %>% 
+  summarize(Mean = mean(SocialDist))
+  
