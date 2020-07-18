2bf855bb09907a2890f6aa994ff041bc43b14028
Alain <vando026@umn.edu>
2020-07-18 23:55:28 +0200

    Dodiffs

--- dofiles/do-figs.R
+++ dofiles/do-figs.R
@@ -114,15 +114,12 @@ dev.off()
 bwrap <- function(y) paste(strwrap(bstate[y], 14), collapse="\n")
 
 # Function to get means and cis
-doReg <- function(RHS, dat) {
-  function(LHS) {
-    mod <- lm(as.formula(paste(LHS, RHS)), data=dat)
+doReg <- function(LHS) {
+    mod <- lm(as.formula(paste(LHS, " ~ TreatList")), data=bdat)
     cf <- round(coef(mod)[2]*100, 1)
     ci <- round(confint(mod)[2, ]*100, 1) 
     return(c(cf, ci))
-  }
 }
-doReg <- doReg_(RHS = " ~   TreatList", bdat)
 
 # Make the data 
 ldat <- getListData(dat_all)
@@ -151,19 +148,117 @@ dev.off()
 ######################################################################
 ####################### PLot Behav means #############################
 ######################################################################
-doReg2 <- doReg_(RHS = "~ -1 + VideoArm:TreatList", ldat)
-
-doReg2("SocialDist")
-
-plotBar <- function(dat) {
-  barplot(as.matrix(dat[, c(1, 7, 3, 9, 5, 11)]))
+doReg2 <- function(LHS) {
+  mod <- lm(as.formula(paste(LHS, "~ -1 + VideoArm:TreatList")), data=ldat)
+  out <- data.frame(summary(mod)$coefficients[, c(1, 2)])
+  colnames(out) <- c("Mn", "SE")
+  out$Arm  <- gsub("VideoArm|:TreatF", "", rownames(out))
+  out$Mn <- round(out$Mn, 3)
+  out$SE  <- round(out$SE, 3)
+  out[c(1, 4, 2, 5, 3, 6), ]
 }
-plotBar(tablist[1, ])
 
 
-tapply(ldat2$SocialDist, list(ldat2$VideoArm, ldat2$TreatList), mean)
-tapply(ldat$SocialDist, list(ldat$VideoArm, ldat$TreatList), mean)
+plotBar <- function(Var) {
+  dat <- doReg2(Var)
+  nms <- rep(c("Control", "Treatment"), 3)
+  bp <- barplot(dat$Mn, ylim=c(0, max(dat$Mn)*1.1), 
+    ylab="Mean score", font.lab=2, main=paste("I will", unlist(bstate[Var])),
+    col=c(rep("lightblue", 2), rep("darkseagreen2", 2), rep("bisque2", 2)))
+  text(bp, y=-0.05, label=nms, xpd=TRUE, adj=c(0.5, 1),
+    cex=1.1)
+  text(bp, dat$Mn, label=formatC(dat$Mn, format="f", digits=2), 
+       cex=0.9, pos=3)
+}
 
-group_by(ldat, VideoArm, TreatList) %>% 
-  summarize(Mean = mean(SocialDist))
-  
+
+mat <- matrix(c(1:6, 7, 7), 4, 2, byrow=TRUE)
+
+png(file.path(output, "BehavMeans.png"),
+  units="in", width=7, height=8.5, pointsize=11, 
+  res=72, type="cairo")
+nf <- layout(mat, heights=c(rep(6, 3), 1))
+par(mai=c(0.4,0.3,0.3,0.1))
+plotBar("SocialDist")
+plotBar("Wash")
+plotBar("StockPile")
+plotBar("CleanDishes")
+plotBar("CleanSurfaces")
+plotBar("UseMedia")
+par(mai=c(0.1,0.1,0.0,0.1))
+plot.new()
+legend("bottom", legend=c("No Video", "APC", "CoVideo"), bty="n", ncol=3, 
+  cex=1.7, fill=c("lightblue", "darkseagreen2", "bisque2"), 
+  text.width=0.15)
+dev.off()
+
+######################################################################
+######################### Diff and Diff ##############################
+######################################################################
+ldat <- getListData(dat_all)
+# trtdat <- filter(ldat, VideoArm != "Control")
+# totdat <- filter(ldat, VideoArm != "Placebo")
+
+# mod1 <- lm(as.formula(paste("SocialDist", "~ -1 + VideoArm:TreatList")), data=trtdat)
+# mod1s <- summary(mod1)
+# mod1c <- coef(mod1s)
+# mod2 <- lm(as.formula(paste("SocialDist", "~ VideoArm*TreatList")), data=trtdat)
+# mod2s <- summary(mod2)
+# mod2c <- coef(mod2s)
+# mod3 <- lm(as.formula(paste("SocialDist", "~ VideoArm*TreatList")), data=totdat)
+# mod3s <- summary(mod3)
+# mod3c <- coef(mod3s)
+
+# mod4 <- lm(as.formula(paste("SocialDist", "~ VideoArm*TreatList")), data=ldat)
+# mod4s <- summary(mod4)
+# mod4c <- coef(mod4s)
+
+eqs <- list(
+  ctrdif = "1*VideoArmControl:TreatList1 - 1*VideoArmControl:TreatList0 = 0",
+  apcdif = "1*VideoArmPlacebo:TreatList1 - 1*VideoArmPlacebo:TreatList0 = 0",
+  trtdif = "1*VideoArmTreatment:TreatList1 - 1*VideoArmTreatment:TreatList0 = 0",
+  toteq = "1*VideoArmTreatment:TreatList1 - 1*VideoArmTreatment:TreatList0 -
+    1*VideoArmControl:TreatList1 + 1*VideoArmControl:TreatList0 = 0",
+  trteq = "1*VideoArmTreatment:TreatList1 - 1*VideoArmTreatment:TreatList0 -
+    1*VideoArmPlacebo:TreatList1 + 1*VideoArmPlacebo:TreatList0 = 0")
+
+lincom <- function(EQ) {
+  lcom <- car::linearHypothesis(mod, EQ)
+  t1 <- attributes(lcom)
+  tdiff <- t1$value[1]
+  tse <- sqrt(t1$vcov[1])
+  tpval <- lcom[2, 6]
+  out <- c(diff=tdiff, se=tse, pval=tpval)
+  round(out, 4)
+}
+
+set3 <- RColorBrewer::brewer.pal(3, "Set2") 
+diffPlot <- function(dat, LHS="") {
+  y <- unlist(dat[1, 1:3])
+  bp <- barplot(y, xaxt="n", ylim=c(0, 0.5),
+    main=paste("This week I will", unlist(bstate[LHS])),
+    ylab="Prevalence", font.lab=2, col=set3)
+  text(x = bp, y=-0.01, label=c("Control", "APC", "CoVideo"), xpd=TRUE, 
+       adj=c(0.5, 1), cex=1.1, srt=0)
+  text(x = bp-0.05, y=y+0.01, label=y,
+       adj=c(1, 0), offset=0.5, cex=0.8)
+  plotCI(bp, y, dat[2, 1:3],
+    add=TRUE, lwd=1, pch=16, col="gray30")
+  pbrack(bp[1], bp[3], 0.40, pval=paste0("Diff = ", dat[1, "toteq"], ", pval = ", dat[3, "toteq"]))
+  pbrack(bp[2], bp[3], 0.48, pval=paste0("Diff = ", dat[1, "trteq"], ", pval = ", dat[3, "trteq"]))
+}
+
+doDiffs <- function(LHS) {
+  mod <- lm(as.formula(paste(LHS, "~ -1 + VideoArm:TreatList")), data=ldat)
+  dat <- data.frame(lapply(eqs, lincom))
+  diffPlot(dat, LHS)
+  return(dat)
+}
+doDiffs("SocialDist")
+
+car::linearHypothesis(mod4, trtdif, verbose=TRUE)
+
+
+tt = car::linearHypothesis(mod4, 
+  "1*VideoArmTreatment:TreatList1 - 1*VideoArmTreatment:TreatList0 -
+  1*VideoArmControl:TreatList1 + 1*VideoArmControl:TreatList0 = 0")
