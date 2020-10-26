a2f6173b9ca91552bbb0ad05737259a75e1f9591
Alain <vando026@umn.edu>
2020-10-26 19:05:01 +0100

    add paper 2 files

---
+++ Paper_2/dofiles/do-analysis.r
@@ -0,0 +1,280 @@
+## Description: Do the analysis
+## Project: COVID-19
+## Author: AV / Created: 20May2020 
+
+#####################################################################
+######################## Demo   #####################################
+#####################################################################
+ getTot <- function(Var) {
+   dat <- group_by(dat_all, VideoArm, .data[[Var]]) %>% tally()
+   group_by(dat, VideoArm) %>% 
+     mutate(Tot = sum(n), Perc=round(n/Tot * 100, 1)) %>% select(-Tot)
+ }
+
+doStat <- function(Var) {
+  Var$n <- format(Var$n, big.mark=",")
+  out <- pivot_wider(Var, names_from=VideoArm, 
+    values_from=c(n, Perc))
+  data.frame(out[, c(1, unlist(Map(c, 2:4, 5:7)))])
+}
+
+dat1 <- lapply(c("Age", "Gender", "Country", "Educ2", "Language"), 
+  getTot)
+tab0 <- lapply(dat1, doStat)
+
+res <- list()
+res$dates <- format(c(min(dat_all$Date), max(dat_all$Date)), "%d %B %Y")
+dtab <- function(x) round(prop.table(table(x))*100,1)
+res$lan <- dtab(dat_all$Language)
+res$ctry <- dtab(dat_all$Country)
+res$educ <- dtab(dat_all$Educ2)
+
+######################################################################
+######################### Knowledge ##################################
+######################################################################
+getTot1 <- function(x, state) {
+  dat <- dat_all[c("VideoArm", x)]
+  correct <- state[[x]][2]
+  yy  <- ifelse(correct=="False", "True", "False")
+  dat[[x]][dat[[x]]=="TimedOut"] <- yy
+  xx <- table(dat)
+  xt <- prop.table(xx, 1)
+  xx <- format(xx, big.mark=",")
+  xt <- xt[, colnames(xt) == correct]
+  xt <- formatC(xt*100, 1, format="f")
+  xx <- data.frame(cbind(xx, pp=xt))
+  xx <- data.frame(xx[1, ], xx[2, ], xx[3, ])
+  xx
+}
+tabc <- data.frame(t(sapply(names(cstate), getTot1, cstate)))
+tabc$Label <- names(cstate)
+rownames(tabc) <- unlist(Map(paste0,
+  c(seq(length(cstate))), c(". "),  
+  sapply(names(cstate), function(x) cstate[[x]][1])))
+tabs <- data.frame(t(sapply(names(sstate), getTot1, sstate)))
+tabs$Label <- names(sstate)
+rownames(tabs) <- unlist(Map(paste0,
+  c(seq(length(cstate) + 1, length(c(cstate, sstate)))), c(". "),  
+  sapply(names(sstate), function(x) sstate[[x]][1])))
+
+res_know <- plotKnow("ClinicTotal", plt=FALSE)
+res_spr <- plotKnow("SpreadTotal", plt=FALSE)
+res_all <- plotKnow("KnowledgeAll", plt=FALSE)
+
+dat_ctrl <- filter(dat_all, VideoArm=="Control")
+dat_trt <- filter(dat_all, VideoArm=="Treatment")
+dat_tot <- filter(dat_all, VideoArm!="Placebo")
+dat_apc <- filter(dat_all, VideoArm!="Control")
+
+reg_clinic <- doRegKnow("ClinicTotal", dat_ctrl)
+reg_clinic_trt <- doRegKnow("ClinicTotal", dat_trt)
+reg_spread <- doRegKnow("SpreadTotal", dat_ctrl)
+reg_spread_trt <- doRegKnow("SpreadTotal", dat_trt)
+reg_all <- doRegKnow("KnowledgeAll", dat_ctrl)
+reg_all_trt <- doRegKnow("KnowledgeAll", dat_trt)
+
+# group_by(dat_ctrl, Country) %>% summarize(NN = mean(ClinicTotal))
+# group_by(dat_trt, Country) %>% summarize(NN = mean(ClinicTotal))
+# lm(ClinicTotal ~ -1 + Country, data=dat_ctrl)
+
+######################################################################
+######################### Get behav in Ctrl ##########################
+######################################################################
+# Get baseline measure of behav intent, remove social desire
+ldat <- getListData(dat_all)
+bdat <- filter(ldat, VideoArm=="Control") 
+behav = data.frame(t(sapply(names(bstate), doRegDirect, bdat)))
+behav <- arrange(behav, TreatList1)
+names(behav) <- c("Est", "LB", "UB")
+bnames <- lapply(rownames(behav), bwrap)
+
+# ldat <- getListData(dat_all)
+# behav = data.frame(t(sapply(names(bstate), getCTR, ldat)))
+# rownames(behav) <- sapply(rownames(behav), function(x) bstate2[[x]])
+
+######################################################################
+######################### Primary outcome ############################
+######################################################################
+ldat <- getListData(dat_all)
+varn <- names(ldat)[-c(1,2)]
+tablist <- data.frame(do.call(rbind, 
+  lapply(setNames(varn, varn), getMeanSE, ldat)))
+
+######################################################################
+######################### Diff #######################################
+######################################################################
+# ldat <- getListData(dat_all)
+# ddat <- lapply(setNames(names(bstate), names(bstate)), doDiffs, ldat)
+
+# pstar <- function(x)  {
+#   if (x < 0.01) return("*")
+#   if (x < 0.05) return("**")
+#   if (x < 0.001) return("***")
+# }
+
+# diffTab <- function(dat) {
+#   browser()
+#   est <- sapply(dat[1, 1:5], 
+#     function(x) formatC(x*100, 2, format="f"))
+#   se <- sapply(dat[2, 1:3], 
+#     function(x) paste0("(", formatC(x*100, 2, format="f"), ")"))
+#   r1 <- c(paste(est[1], se[1]), paste(est[2], se[2]), paste(est[3], se[3]))
+#   difp <- c(paste0(est[4], pstar(dat[3, 4])), paste0(est[5], pstar(dat[3, 5])))
+#   c(r1, difp)
+# }
+# difftable <- do.call(rbind, lapply(ddat, diffTab))
+
+
+modc <- lm(ClinicTotal ~ - 1 + Age:VideoArm, data=dat_tot)
+mods <- lm(SpreadTotal ~ - 1 + Age:VideoArm, data=dat_tot)
+moda <- lm(KnowledgeAll ~ - 1 + Age:VideoArm, data=dat_tot)
+
+c1824 = "1*Age18-24 years:VideoArmTreatment - 1*Age18-24 years:VideoArmControl = 0"
+c2534 = "1*Age25-34 years:VideoArmTreatment - 1*Age25-34 years:VideoArmControl = 0"
+c3544 = "1*Age35-44 years:VideoArmTreatment - 1*Age35-44 years:VideoArmControl = 0"
+c4554 = "1*Age45-54 years:VideoArmTreatment - 1*Age45-54 years:VideoArmControl = 0"
+c5559 = "1*Age55-59 years:VideoArmTreatment - 1*Age55-59 years:VideoArmControl = 0"
+cnm <- ls(pattern="^c[1-9]") 
+agediff <- data.frame(
+  sapply(setNames(c(c1824, c2534, c3544, c4554, c5559), cnm), lincom, modc))
+agediffs <- data.frame(
+  sapply(setNames(c(c1824, c2534, c3544, c4554, c5559), cnm), lincom, mods))
+agediffa <- data.frame(
+  sapply(setNames(c(c1824, c2534, c3544, c4554, c5559), cnm), lincom, moda))
+
+modc <- lm(ClinicTotal ~ - 1 + Age:VideoArm, data=dat_apc)
+mods <- lm(SpreadTotal ~ - 1 + Age:VideoArm, data=dat_apc)
+moda <- lm(KnowledgeAll ~ - 1 + Age:VideoArm, data=dat_apc)
+c1824 = "1*Age18-24 years:VideoArmTreatment - 1*Age18-24 years:VideoArmPlacebo = 0"
+c2534 = "1*Age25-34 years:VideoArmTreatment - 1*Age25-34 years:VideoArmPlacebo = 0"
+c3544 = "1*Age35-44 years:VideoArmTreatment - 1*Age35-44 years:VideoArmPlacebo = 0"
+c4554 = "1*Age45-54 years:VideoArmTreatment - 1*Age45-54 years:VideoArmPlacebo = 0"
+c5559 = "1*Age55-59 years:VideoArmTreatment - 1*Age55-59 years:VideoArmPlacebo = 0"
+cnm <- ls(pattern="^c[1-9]") 
+agediff_apc <- data.frame(
+  sapply(setNames(c(c1824, c2534, c3544, c4554, c5559), cnm), lincom, modc))
+agediffs_apc <- data.frame(
+  sapply(setNames(c(c1824, c2534, c3544, c4554, c5559), cnm), lincom, mods))
+agediffa_apc <- data.frame(
+  sapply(setNames(c(c1824, c2534, c3544, c4554, c5559), cnm), lincom, moda))
+
+
+save(tab0, flow, res, tabc, tabs, res_know, res_spr, res_all,
+  reg_clinic, reg_clinic_trt, reg_spread, reg_spread_trt, 
+  reg_all, reg_all_trt, behav, 
+  cstate, sstate, bstate, agediff, agediffs, agediffa, 
+  file=file.path(output, "Results.RData"))
+
+
+######################################################################
+######################### List Intent ################################
+######################################################################
+plotList <- function(LHS, yLim=c(0, 1.0), dat=dat_all) {
+
+  dat_ctrl <- filter(dat_all, VideoArm=="Control")
+  dat_ctrl <- mutate(dat_ctrl, 
+    Treat  = as.numeric(dat_ctrl$ListArm=="List Treatment"),
+    EducNum = as.numeric(as.factor(Educ2)))
+  fdat <- filter(dat_ctrl, Gender=="Female")
+  mdat <- filter(dat_ctrl, Gender=="Male")
+
+
+  fmod <- ictreg(as.formula(paste(LHS, "~ -1 + Age")), data=data.frame(fdat),
+    treat = "Treat", J=5, method= "lm")
+  mmod <- ictreg(as.formula(paste(LHS, "~ -1 + Age")), data=data.frame(mdat),
+    treat = "Treat", J=5, method= "lm")
+
+  fres <- summary(fmod)
+  fcoefs <- fres$par.treat
+  fcoef_age <- fcoefs[grepl("Age", names(fcoefs))] 
+  print(fres)
+
+  # ndat <- data.frame(Age = sort(unique(dat_all$Age)), EducNum=3)
+  # predict(fmod, ndat)
+
+  mres <- summary(mmod)
+  print(mres)
+  mcoefs <- mres$par.treat
+  mcoef_age <- mcoefs[grepl("Age", names(mcoefs))] 
+  x <- seq(length(fcoefs))
+
+  plot(x, fcoef_age, ylim=yLim, bty="l", type="n", ylab="Estimated proportion",
+       xlab="Age ", main=paste("This week I will ", bwrap(LHS, 30)), font.lab=2, xaxt="n")
+  lines(ksmooth(x, mcoef_age, "normal", bandwidth=2, n.points=200), lwd=2.0, lty=1, col=set3[3])
+  lines(ksmooth(x, fcoef_age, "normal", bandwidth=2, n.points=200), lwd=2.0, lty=2, col=set3[2])
+  axis(1, x,  gsub("(Age)", "", names(mcoef_age)))
+  legend("bottom", legend=c("Men", "Women"), lty=c(1, 2), col=c(set3[3], set3[2]), 
+         ncol=2, bty="n", lwd=3 )
+}
+
+png(file.path(output, paste0("List_", "1", ".png")),
+  units="in", width=7, height=8, pointsize=10, 
+  res=500, type="cairo")
+par(mfrow=c(3, 2))
+plotList("SocialDist")
+plotList("StockPile")
+plotList("UseMedia")
+plotList("CleanDishes")
+plotList("CleanSurfaces")
+plotList("Wash")
+dev.off()
+
+
+######################################################################
+######################### Get Video ##################################
+######################################################################
+dat <- readr::read_csv(file=file.path(datapath, "Derived/VideoTime.csv"))
+dat <- filter(dat, ID=="5e652009b1074229c1815729")
+
+
+
+
+
+######################################################################
+######################### ############################################
+######################################################################
+# # debugonce(doEffects)
+# socdist <- doEffects("SocialDist")
+# doEffects("Wash")
+
+# APCCtrlX =  filter(ldat2, VideoArm=="Placebo" & TreatList==0)  %>% summarize(mean(SocialDist))
+# APCTrtX =  filter(ldat2, VideoArm=="Placebo" & TreatList==1)  %>% summarize(mean(SocialDist))
+# VidCtrlX =  filter(ldat2, VideoArm=="Treatment" & TreatList==0)  %>% summarize(mean(SocialDist))
+# VidTreatX = filter(ldat2, VideoArm=="Treatment" & TreatList==1) %>% summarize(mean(SocialDist))
+# tapply(ldat2$SocialDist, list(ldat2$VideoArm, ldat2$TreatList), mean)
+
+
+# mod = lm(SocialDist ~ VideoArm*TreatList, data=ldat2)
+# summary(mod)
+# coefs <- coef(mod) 
+# APCCtrl_ = coefs["(Intercept)"]
+# APCTrt_ <- coefs["(Intercept)"] + coefs["TreatList"]
+# VidCtrl_ <- coefs["(Intercept)"] + coefs["VideoArmTreatment"]
+# VidTrt_ <- coefs["(Intercept)"] + coefs["VideoArmTreatment"] + coefs["TreatList"] + coefs["VideoArmTreatment:TreatList"]
+
+# lapply(ls(pattern="_$"), function(x) get(x))
+# lapply(ls(pattern="X$"), function(x) get(x))
+
+# # Difference in difference
+# coefs["VideoArmTreatment:TreatList"]
+# (VidTreatX - VidCtrlX) - (APCTrtX - APCCtrlX)
+# tapply(ldat$SocialDist, list(ldat$VideoArmF, ldat$TreatListF), sd)
+
+# debugonce(doEffects)
+# doEffects("CleanDishes")
+
+# bedat <- select(dat_all, ID, VideoArm, TreatList, starts_with("BE"))
+# bedat <- mutate(bedat, 
+#   VideoArm = as.factor(VideoArm), TreatList = as.factor(TreatList))
+# trt_bedat <- filter(bedat, VideoArm != "Control")
+
+# mod = glm(BESocialDist ~ VideoArm*TreatList, data=trt_bedat, family=poisson(link="log"))
+# summary(mod)$coefficients
+
+# doEffects("SocialDist", dat=bedat, model=glm, family=poisson)
+
+
+
+# adat <- filter(dat_all, !is.na(ClinicTotal1) | !is.na(SpreadTotal1)) %>% 
+#   select(ClinicTotal1, SpreadTotal1, VideoArm)
+
---
+++ Paper_2/dofiles/do-figs.R
@@ -0,0 +1,121 @@
+## Description: Do figures
+## Project: CoVideo
+## Author: AV / Created: 18Jul2020 
+
+load(file.path(output, "Results.RData"))
+
+######################################################################
+######################### KNowledge ##################################
+######################################################################
+plotKnow("ClinicTotal", "Clinical knowledge of COVID-19 (10 items)", 
+  yLim=c(9.15, 9.39), ppos=c(9.29, 9.338, 9.38))
+
+plotKnow("SpreadTotal", "Knowledge of preventing COVID-19 spread (8 items)", 
+  yLim=c(7.62, 7.75), ppos=c(7.715, 7.695, 7.74), H=0.005)
+
+
+png(file.path(output, paste0("KnowledgeAll", ".png")),
+  units="in", width=5, height=5.0, pointsize=9, 
+  res=500, type="cairo")
+plotKnow("KnowledgeAll", "COVID-19 Knowledge (18 items)", 
+ yLim=c(16.8, 17.10), ppos=c(16.95, 17.005, 17.06), write=FALSE, 
+ yaxt="n")
+axis(2, at=seq(16.80, 17.10, by=0.10), las=1)
+dev.off()
+
+######################################################################
+######################### Behave Direct ##############################
+######################################################################
+png(file.path(output, "BehavIntent.png"),
+  units="in", width=6.5, height=5.0, pointsize=10, 
+  res=500, type="cairo")
+bp <- barplot(behav$Est, xaxt="n", ylim=c(0, 100),
+  main="This week, I will ...", ylab="Percentage", font.lab=2,
+  col="darkslategray3", border="darkslategray4")
+bnames <- lapply(rownames(behav), bwrap)
+text(x = bp, y=-1, label=bnames, xpd=TRUE, 
+     adj=c(0.5, 1), cex=0.8, srt=0)
+text(x = bp-0.05, y=behav$Est+1, label=behav$Est,
+     adj=c(1, 0), offset=0.5, cex=0.8)
+plotCI(bp, behav$Est, li=behav$LB, ui=behav$UB, 
+  main="Behavioral Intent", bty="n", add=TRUE,
+  ylim=c(0, 100), lwd=1, pch=16, 
+  xlab="Trial arm", xaxt="n", col="gray30")
+dev.off()
+
+
+######################################################################
+####################### PLot Behav means #############################
+######################################################################
+ldat <- getListData(dat_all)
+plotBar <- function(Var, Data=ldat) {
+  dat <- doRegIndirect(Var, Data)
+  nms <- rep(c("Control", "Treatment"), 3)
+  bp <- barplot(dat$Mn, ylim=c(0, max(dat$Mn)*1.1), 
+    ylab="Mean score", font.lab=2, main=paste("I will", unlist(bstate[Var])),
+    col=c(rep("lightblue", 2), rep("darkseagreen2", 2), rep("bisque2", 2)))
+  text(bp, y=-0.05, label=nms, xpd=TRUE, adj=c(0.5, 1),
+    cex=1.1)
+  text(bp, dat$Mn, label=formatC(dat$Mn, format="f", digits=2), 
+       cex=0.9, pos=3)
+}
+
+mat <- matrix(c(1:6, 7, 7), 4, 2, byrow=TRUE)
+png(file.path(output, "BehavMeans.png"),
+  units="in", width=7, height=8.5, pointsize=11, 
+  res=72, type="cairo")
+nf <- layout(mat, heights=c(rep(6, 3), 1))
+par(mai=c(0.4,0.3,0.3,0.1))
+plotBar("SocialDist")
+plotBar("Wash")
+plotBar("StockPile")
+plotBar("CleanDishes")
+plotBar("CleanSurfaces")
+plotBar("UseMedia")
+par(mai=c(0.1,0.1,0.0,0.1))
+plot.new()
+legend("bottom", legend=c("No Video", "APC", "CoVideo"), bty="n", ncol=3, 
+  cex=1.7, fill=c("lightblue", "darkseagreen2", "bisque2"), 
+  text.width=0.15)
+dev.off()
+
+######################################################################
+######################### Diff and Diff ##############################
+######################################################################
+btitle <- lapply(setNames(names(bstate), names(bstate)), bwrap, 25)
+
+diffPlot <- function(dat, LHS="", yLim, yvals=NULL) {
+  nm <- names(dat); dat <- dat[[1]]
+  y <- unlist(dat[1, 1:3]) * 100
+  plotCI(1:3, y, dat[2, 1:3]*100, cex.lab=1.2,
+    bty="n", ylim=yLim, xaxt="n", xlab="", ylab="Mean ",
+    lwd=3, pch=16, col=set3, font.lab=2, cex.axis=1.15)
+  title(paste("This week I will", unlist(btitle[nm])), cex.main=1.3)
+  axis(1, at=1:3, label=c("Control", "APC", "CoVideo"), cex.axis=1.2, font=2)
+  text(x = c(1, 2, 2.77) + 0.025, y=y + 0.5, 
+    label=formatC(y, format="f", digits=1),
+    adj=c(0, 0), cex=1.0)
+  abline(h=y[1], lwd=1, lty=2, col="grey70")
+  pbrack(1, 3, yvals[1], h=1, CEX = 1.1,
+    pval=paste0("Tot. Diff = ", fmt(dat[1, "toteq"]*100, 1), ", pval = ", 
+    fmt(dat[3, "toteq"], 3)))
+  pbrack(2,  3, yvals[2], h=1, CEX = 1.1,
+    pval=paste0("Trt. Diff = ", fmt(dat[1, "trteq"]*100, 1), ", pval = ", 
+    fmt(dat[3, "trteq"], 3)))
+}
+ddat <- lapply(setNames(names(bstate), names(bstate)), doDiffs)
+
+mat <- matrix(c(1:6), 3, 2, byrow=TRUE)
+png(file.path(output, "BehavDiffs.png"),
+  units="in", width=7, height=9.5, pointsize=12, 
+  res=300, type="cairo")
+nf <- layout(mat, heights=rep(10, 3))
+par(mai=c(0.4,0.6,0.4,0.2))
+diffPlot(ddat["SocialDist"], yLim=c(10, 42), yvals=c(40, 35))
+diffPlot(ddat["Wash"], yLim=c(80, 104), yvals=c(102, 98))
+diffPlot(ddat["StockPile"], yLim=c(20, 46), yvals=c(45, 41))
+diffPlot(ddat["CleanDishes"], yLim=c(80, 104), yvals=c(102, 98))
+diffPlot(ddat["CleanSurfaces"], yLim=c(65, 90), yvals=c(89, 85))
+diffPlot(ddat["UseMedia"], yLim=c(15, 41), yvals=c(40, 36))
+dev.off()
+
